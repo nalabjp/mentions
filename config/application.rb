19bc321f618ab0a31ae3eb6eb16c0b3549c3cdd1
umisora <bit72s_akm@hotmail.com>
2017-07-03 23:07:47 +0900

    :up: fix deplicate warning

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Mentions
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.serve_static_files = true
+    config.public_file_server.enabled = true
   end
 end
